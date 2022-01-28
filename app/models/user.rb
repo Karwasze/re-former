b3f45ed868a570a434111743ab4b23e8e6bb5386
Karol Kędzierski <karolkedzierski@live.com>
2022-01-28 23:51:11 +0100

    Before Railsy Forms with #form_tag

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    validates :name, presence: true, length: { maximum: 50 }
-    validates :email, presence: true, length: { maximum: 255 },
-    validates :password, presence: true, length: { minimum: 6 }
+    validates :username, presence: true, length: { maximum: 50 }
+    validates :email, presence: true, length: { maximum: 255 }
+    validates :password, presence: true, length: { minimum: 4 }
 end
